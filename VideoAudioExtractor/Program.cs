c9968ebbb670ff87a9dfb0fd494bea417c968ce3
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2020-07-29 18:22:48 +0300

    Clean everything, start over with console app

---
+++ VideoAudioExtractor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VideoAudioExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
