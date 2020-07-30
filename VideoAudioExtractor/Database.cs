f39dcabaddd5f1a5a0c699ceae9269bb047e9cb0
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2020-07-30 16:30:01 +0300

    Base db class and recording object class

---
+++ VideoAudioExtractor/Database.cs
@@ -0,0 +1,50 @@
+﻿using Npgsql;
+
+namespace VideoAudioExtractor
+{
+    public class Database
+    {
+        // Database
+        private NpgsqlConnection _connection = null;
+
+        // Variables
+
+
+        public Database(string connString)
+        {
+            OpenDatabaseConnection(connString);
+        }
+
+
+        private async void OpenDatabaseConnection(string connString)
+        {
+            _connection = new NpgsqlConnection(connString);
+            await _connection.OpenAsync();
+        }
+
+
+        private async void InsertRecording()
+        {
+        }
+
+        private async void GetRecordings()
+        {
+        }
+
+
+        /*
+         // Insert some data
+        await using (var cmd = new NpgsqlCommand("INSERT INTO data (some_field) VALUES (@p)", conn))
+        {
+            cmd.Parameters.AddWithValue("p", "Hello world");
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        // Retrieve all rows
+        await using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
+        await using (var reader = await cmd.ExecuteReaderAsync())
+            while (await reader.ReadAsync())
+                Console.WriteLine(reader.GetString(0));
+         */
+    }
+}
---
+++ VideoAudioExtractor/Recording.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+
+namespace VideoAudioExtractor
+{
+    public class Recording
+    {
+        // Variables
+        private readonly BigInteger _id;
+        private readonly string _fileName;
+        private readonly string _startTime;
+        private readonly string _endTime;
+
+        // Constructor
+        public Recording(BigInteger id, string fileName, string startTime, string endTime)
+        {
+            _id = id;
+            _fileName = fileName;
+            _startTime = startTime;
+            _endTime = endTime;
+        }
+
+        public BigInteger GetId()
+        {
+            return _id;
+        }
+
+        public string GetFileName()
+        {
+            return _fileName;
+        }
+
+        public string GetStartTime()
+        {
+            return _startTime;
+        }
+
+        public string GetEndTime()
+        {
+            return _endTime;
+        }
+    }
+}
